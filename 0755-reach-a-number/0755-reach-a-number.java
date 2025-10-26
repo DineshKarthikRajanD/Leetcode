2cfd33272c71e1e58fef16485bece604b6eba0e3
DineshKarthikRajanD <167109206+DineshKarthikRajanD@users.noreply.github.com>
2025-10-26 11:05:28 +0530

    Time: 1 ms (83.83%), Space: 40.5 MB (42.23%) - LeetHub

---
+++ 0755-reach-a-number/0755-reach-a-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reachNumber(int target) {
+        int sum = 0, step = 0;
+        target = Math.abs(target);
+        
+        while(sum < target){
+            step++;
+            sum += step;
+        }
+
+        while((sum - target) % 2 != 0){
+            step++;
+            sum += step;
+        }
+
+        return step;
+    }
+}
