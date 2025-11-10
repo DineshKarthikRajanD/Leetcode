66160ad1ef309c9de40c0305756d5e0f8c31bc1b
DineshKarthikRajanD <167109206+DineshKarthikRajanD@users.noreply.github.com>
2025-11-10 21:29:45 +0530

    Time: 1 ms (94.34%), Space: 46.4 MB (8.07%) - LeetHub

---
+++ 4058-compute-alternating-sum/4058-compute-alternating-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum = 0, n = nums.length;
+
+        for(int i=0;i<n;i++){
+            if(i % 2 == 0){
+                sum += nums[i];
+            }
+            else{
+                sum -= nums[i];
+            }
+        }
+
+        return sum;
+    }
+}
