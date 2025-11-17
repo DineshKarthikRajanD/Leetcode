074477e7653fbf95cb1c58c1938103a75010c762
DineshKarthikRajanD <167109206+DineshKarthikRajanD@users.noreply.github.com>
2025-11-17 20:48:33 +0530

    Time: 14 ms (25.16%), Space: 48.3 MB (5.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,22 +1,18 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n = nums.length;
-        int count[] = new int [n+1];
-
-        for(int i=0;i<n;i++){
-            count[nums[i]]++;
-        }
-
         int res[] = new int [2];
-
-        for(int i=1;i<count.length;i++){
-            if(count[i] > 1){
-                res[0] = i;
+        Arrays.sort(nums);
+        int s = 1;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(i > 0 && nums[i-1] == nums[i]){
+                res[0] = nums[i];
             }
-            else if(count[i] == 0){
-                res[1] = i;
+            if(nums[i] == s){
+                s++;
             }
         }
+        res[1] = s;
         return res;
     }
 }
