008f50bc8adf4d8b5b89b22627abeba613e1e26c
DineshKarthikRajanD <167109206+DineshKarthikRajanD@users.noreply.github.com>
2025-11-28 09:59:14 +0530

    Time: 5 ms (3.4%), Space: 42.5 MB (9.43%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxValue(int n, int index, int maxSum) {
+        int res = 1;
+        maxSum -= n;
+        int left = 0, right = 0;
+        int maxLeft = index, maxRight = n - index - 1;
+
+        while(maxSum > 0) {
+            res++;
+            int leftVal = Math.min(left++, maxLeft);
+            int rightVal = Math.min(right++, maxRight);
+            maxSum -= (1 + leftVal + rightVal);
+
+            if(leftVal == maxLeft && rightVal == maxRight) {
+                break;
+            }
+        }
+
+        if(maxSum > 0){
+            res = res + (maxSum/n);
+        }
+
+        return (maxSum<0) ? res-1 : res;
+    }
+}
