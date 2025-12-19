48b8d27df90c63ccafc9797040352f83f728da94
DineshKarthikRajanD <167109206+DineshKarthikRajanD@users.noreply.github.com>
2025-12-19 22:08:45 +0530

    Time: 0 ms (100%), Space: 45.3 MB (35.23%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int n = nums.length, neg = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0) return 0;
+            if(nums[i] < 0) neg++;
+        }
+        return (neg % 2 == 0) ? 1 : -1;
+    }
+}
